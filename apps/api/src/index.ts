90f51b84cbab9ee5be8c3ea48f518bc78a944f63
Nogu-y <yoichinogu5@gmail.com>
2026-04-30 18:16:09 +0900

    fix(api): APIが起動しない問題を修正

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from 'hono'
+import { cors } from "hono/cors"
 import { registerOpenAPIRoutes } from './openapi'
 import { systemRoutes } from './routes/system'
 import { serve } from '@hono/node-server'
 import { apiEnv } from './env'
 
 export const app = new Hono()
+  .use("/*", cors())
   .route('/', systemRoutes)
 
 registerOpenAPIRoutes(app)
