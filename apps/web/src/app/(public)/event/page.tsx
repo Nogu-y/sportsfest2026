076545c4b55b6a02a1e10c302c4d77a693553edf
h-koichiro <h.koichiro0206@gmail.com>
2026-05-07 03:58:30 +0900

    feat: EventHeroコンポーネントを追加し、リンクのテキストを修正

--- apps/web/src/app/(public)/event/page.tsx
+++ apps/web/src/app/(public)/event/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import HeaderEventCardList from "src/components/common/HeaderEventCardList";
+import EventHero from "src/components/layouts/eventHero/EventHero";
 import SubHeader from "src/components/layouts/subheader/SubHeader";
 
 export default function Events() {
@@ -8,9 +9,9 @@ export default function Events() {
       <SubHeader>
         <HeaderEventCardList />
       </SubHeader>
-
+      <EventHero>バレーボール</EventHero>
       <main className="p-6">
-        <Link href={"/sample"}>samplaae</Link>
+        <Link href={"/sample"}>sample</Link>
       </main>
     </>
   );
