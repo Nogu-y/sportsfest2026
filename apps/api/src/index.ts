afe56f708d5eb37226c07f3446eb94923c9f0a5a
Nogu-y <yoichinogu5@gmail.com>
2026-05-06 12:38:27 +0900

    feat(api): /api/public/*をルーティング

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,13 +1,15 @@
 import { Hono } from 'hono'
 import { cors } from "hono/cors"
 import { registerOpenAPIRoutes } from './openapi'
+import { publicRoutes } from './routes/public'
 import { systemRoutes } from './routes/system'
 import { serve } from '@hono/node-server'
 import { apiEnv } from './env'
 
 export const app = new Hono()
   .use("/*", cors())
-  .route('/', systemRoutes)
+  .route('/api/system', systemRoutes)
+  .route('/api/public', publicRoutes)
 
 registerOpenAPIRoutes(app)
 
