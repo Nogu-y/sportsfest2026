6b56907a1f8565eda38279f5b82cdf6843fc8550
Nogu-y <yoichinogu5@gmail.com>
2026-05-07 23:24:12 +0900

    feat(db): サンプルデータの投入とデータ投入環境の整備

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -3,7 +3,7 @@ import { apiEnv } from './src/env'
 
 export default defineConfig({
   dialect: 'postgresql',
-  schema: './src/db/schema.ts',
+  schema: ['./src/db/schema.ts', './src/db/enums.ts'],
   out: './drizzle',
   dbCredentials: {
     url: apiEnv.DATABASE_URL
