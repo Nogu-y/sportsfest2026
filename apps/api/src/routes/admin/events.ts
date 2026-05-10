3eb61eca22e99ce3a8fcd07daecb2f21e545694e
NeeeRL <kenken.s5049@gmail.com>
2026-05-10 22:02:39 +0900

    style: インデントやコードの見た目を整えた

--- apps/api/src/routes/admin/events.ts
+++ apps/api/src/routes/admin/events.ts
@@ -3,20 +3,17 @@ import { getEvents } from '../../repositories/admin/events'
 
 export const adminEventsRoutes = new OpenAPIHono
 
+// 少し見やすく
 adminEventsRoutes.get( '/', async (c) => {
   try {
     const data = await getEvents()
     return c.json(data, 200)
   } catch (error) {
-  console.error(error)
+    console.error(error)
 
-  return c.json(
-    {
-      message:
-        'データ取得に失敗しました'
-    },
-    500
-  )
-}
+    return c.json({
+      message: 'データ取得に失敗しました'
+    }, 500)
+  }
 })
 
