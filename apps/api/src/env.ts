1ce1f2ddee2ba1981de9c638c649fda0120f6ac0
Nogu-y <yoichinogu5@gmail.com>
2026-05-11 22:36:39 +0900

    feat(api): 認証で使用する環境変数関連を追加

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -5,5 +5,7 @@ export const apiEnv = apiEnvSchema.parse({
   POSTGRES_DB: process.env.POSTGRES_DB,
   POSTGRES_USER: process.env.POSTGRES_USER,
   POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
-  DATABASE_URL: process.env.DATABASE_URL
+  DATABASE_URL: process.env.DATABASE_URL,
+  AUTH_PASSWORD_PEPPER: process.env.AUTH_PASSWORD_PEPPER,
+  AUTH_SESSION_COOKIE_NAME: process.env.AUTH_SESSION_COOKIE_NAME
 })
