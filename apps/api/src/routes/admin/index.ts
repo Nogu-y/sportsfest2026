05f910970678e0e8e6ed4bbf29b743d160887358
NeeeRL <kenken.s5049@gmail.com>
2026-05-13 14:55:32 +0900

    feat(matches_api): admin_matches(api)を実装

--- apps/api/src/routes/admin/index.ts
+++ apps/api/src/routes/admin/index.ts
@@ -1,5 +1,8 @@
 import { OpenAPIHono } from '@hono/zod-openapi'
 import { adminEventsRoutes } from './events'
+import { adminMatchesRoutes } from './matches'
 
-export const adminRoutes = new OpenAPIHono().route( '/events', adminEventsRoutes)
+export const adminRoutes = new OpenAPIHono()
+  .route('/events', adminEventsRoutes)
+  .route('/matches', adminMatchesRoutes)
 
