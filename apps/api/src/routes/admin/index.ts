3984754ddbce6f85e83826fe6d299dc179b67ac0
h-koichiro <h.koichiro0206@gmail.com>
2026-05-14 20:18:38 +0900

    重複していた /admin/events マウントを1つに整理

--- apps/api/src/routes/admin/index.ts
+++ apps/api/src/routes/admin/index.ts
@@ -3,6 +3,5 @@ import { adminEventsRoutes } from './events'
 import { adminLocationsRoutes } from './locations'
 
 export const adminRoutes = new OpenAPIHono()
-    .route( '/events', adminEventsRoutes)
-    .route('/events', adminEventsRoutes)
-    .route('/locations', adminLocationsRoutes)
+  .route('/events', adminEventsRoutes)
+  .route('/locations', adminLocationsRoutes)
