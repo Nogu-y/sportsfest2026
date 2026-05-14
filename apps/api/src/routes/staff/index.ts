3e7bd737b4b1e67a06c91fb4aa350f79b4bf9008
h-koichiro <h.koichiro0206@gmail.com>
2026-05-14 20:18:49 +0900

    消えていた /staff/events のマウントを復旧

--- apps/api/src/routes/staff/index.ts
+++ apps/api/src/routes/staff/index.ts
@@ -1,5 +1,7 @@
 import { OpenAPIHono } from '@hono/zod-openapi'
+import { staffEventRoutes } from './events'
 import { staffMatchesRoutes } from './matches'
 
 export const staffRoutes = new OpenAPIHono()
+  .route('/events', staffEventRoutes)
   .route('/matches', staffMatchesRoutes)
