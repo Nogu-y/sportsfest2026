d5efbd6b6057177eab01995cbc4d5f27f68c1d06
Nogu-y <yoichinogu5@gmail.com>
2026-05-18 11:37:31 +0900

    feat(api): バックエンドに認証を適用

--- apps/api/src/routes/admin/index.ts
+++ apps/api/src/routes/admin/index.ts
@@ -5,8 +5,10 @@ import { adminEventBlocksRoutes } from './eventBlocks'
 import { adminMatchesRoutes } from './matches'
 import { adminLocationsRoutes } from './locations'
 import { adminUsersRoutes } from './users'
+import { requireAdminRole } from '../../middleware/staffAuth'
 
 export const adminRoutes = new OpenAPIHono()
+  .use('/*', requireAdminRole)
   .route('/events', adminEventsRoutes)
   .route('/event-blocks', adminEventBlocksRoutes)
   .route('/matches', adminMatchesRoutes)
