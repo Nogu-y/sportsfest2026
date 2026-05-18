be79d3bb6ef8eb24b3aaffbf24cbc9e92577e4e2
SHO800 <fg582323@docomo.ne.jp>
2026-05-18 21:31:30 +0900

    feat(web): (public)以下にComingSoon適用

--- apps/web/src/app/(public)/layout.tsx
+++ apps/web/src/app/(public)/layout.tsx
@@ -1,12 +1,14 @@
 import {ReactNode} from "react";
 import Header from "../../components/layouts/header/Header";
+import {ComingSoon} from "../../components/layouts/messagePage/ComingSoon";
 
 export default function PublicLayout ({children, modal}: { children: ReactNode, modal: ReactNode }) {
     return (
         <div className={"scrollbar-none"}>
-            <Header/>
-            {children}
-            {modal}
+            <ComingSoon />
+            {/*<Header/>*/}
+            {/*{children}*/}
+            {/*{modal}*/}
         </div>
     )
 }
