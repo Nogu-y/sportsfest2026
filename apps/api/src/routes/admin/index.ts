7d008e368100f729d73f664a2e002255e19a6148
Nogu-y <yoichinogu5@gmail.com>
2026-05-18 09:56:04 +0900

    feat(api): event-blockのCURDを追加

--- apps/api/src/routes/admin/index.ts
+++ apps/api/src/routes/admin/index.ts
@@ -1,11 +1,13 @@
 import { OpenAPIHono } from '@hono/zod-openapi'
 import { TeamsRoutes } from './teams.js'
 import { adminEventsRoutes } from './events'
+import { adminEventBlocksRoutes } from './eventBlocks'
 import { adminMatchesRoutes } from './matches'
 import { adminLocationsRoutes } from './locations'
 
 export const adminRoutes = new OpenAPIHono()
   .route('/events', adminEventsRoutes)
+  .route('/event-blocks', adminEventBlocksRoutes)
   .route('/matches', adminMatchesRoutes)
   .route('/locations', adminLocationsRoutes)
   .route('/teams', TeamsRoutes)
