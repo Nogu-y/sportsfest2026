f8f332939cbcd75d939a0fb687f462b6c82ecf2a
Nogu-y <yoichinogu5@gmail.com>
2026-05-19 23:39:49 +0900

    feat(frontend): 認証時にPublicページを表示するように修正

--- apps/web/src/app/(public)/layout.tsx
+++ apps/web/src/app/(public)/layout.tsx
@@ -1,12 +1,15 @@
 import {ReactNode} from "react";
 import Header from "../../components/layouts/header/Header";
+import { PublicPageGate } from '../../components/auth/PublicPageGate'
 
 export default function PublicLayout({children, modal}: { children: ReactNode, modal: ReactNode }) {
     return (
         <div className={"scrollbar-none"}>
-            <Header/>
-            {children}
-            {modal}
+            <PublicPageGate>
+                <Header/>
+                {children}
+                {modal}
+            </PublicPageGate>
         </div>
     )
 }
