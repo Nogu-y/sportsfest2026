5811eb5b0b0c28a6fb1202650c1f72801e43fd09
SHO800 <fg582323@docomo.ne.jp>
2026-05-20 00:56:09 +0900

    feat(web): 各種目の競技ルール及び概要を追加

--- apps/web/src/components/layouts/eventHero/EventHero.tsx
+++ apps/web/src/components/layouts/eventHero/EventHero.tsx
@@ -2,14 +2,15 @@ import React, {ReactNode} from "react";
 import EventRulePopup from "./EventRulePopup";
 
 type EventHeroProps = {
+    eventId: number;
     children: ReactNode;
 };
 
-const EventHero = ({children}: EventHeroProps) => {
+const EventHero = ({eventId, children}: EventHeroProps) => {
     return (
         <div className="relative  flex items-center justify-center -mb-1 bg-[#2d5a8e]">
             {children}
-            <EventRulePopup ruleId="sample"/>
+            <EventRulePopup eventId={eventId}/>
         </div>
     );
 };
