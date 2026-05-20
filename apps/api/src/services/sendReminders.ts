555c0118263085d96d880824e4ac4c1d8136f492
SHO800 <fg582323@docomo.ne.jp>
2026-05-21 03:28:20 +0900

    Revert "fix(chore): 不要なiconを削除"
    This reverts commit 4d2a4503b892be3d962d8931328dba59b3590373.

--- apps/api/src/services/sendReminders.ts
+++ apps/api/src/services/sendReminders.ts
@@ -107,6 +107,8 @@ export async function sendMatchReminders() {
             title: `試合開始まであと${minutesLeft}分！`,
             body: `【${matchVersus}】${eventDisplay} がまもなく開始します${matchNumberDisplay}`,
             url: `/match/${target.matchId}`,
+              icon: '/web-app-manifest-192x192.png',
+              
           }),
             {
                 headers: {
